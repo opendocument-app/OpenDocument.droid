22f6a97eda82daf29c8ec46d4833c46c22968847
Thomas Taschauer <tomtasche@gmail.com>
2012-10-18 18:07:43 +0200

    update intro; revert package renaming; delete stuff/

---
+++ src/at/tomtasche/reader/background/AndroidFileCache.java
@@ -0,0 +1,38 @@
+package at.tomtasche.reader.background;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import android.content.Context;
+import android.net.Uri;
+import at.andiwand.odf2html.translator.File2URITranslator;
+import at.andiwand.odf2html.translator.FileCache;
+
+public class AndroidFileCache extends FileCache {
+
+	private static File2URITranslator DEFAULT_INSTANCE = new File2URITranslator() {
+		@Override
+		public URI translate(File file) {
+			URI uri = file.toURI();
+
+			File imageFile = new File(uri);
+			String imageFileName = imageFile.getName();
+
+			URI result = null;
+
+			try {
+				result = new URI("content://at.tomtasche.reader/"
+						+ Uri.encode(imageFileName));
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			}
+
+			return result;
+		}
+	};
+
+	public AndroidFileCache(Context context) {
+		super(context.getCacheDir(), DEFAULT_INSTANCE, false);
+	}
+}
---
+++ src/at/tomtasche/reader/background/Document.java
@@ -0,0 +1,112 @@
+package at.tomtasche.reader.background;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Document implements Parcelable {
+
+	public static final Parcelable.Creator<Document> CREATOR = new Parcelable.Creator<Document>() {
+		public Document createFromParcel(Parcel in) {
+			return new Document(in);
+		}
+
+		public Document[] newArray(int size) {
+			return new Document[size];
+		}
+	};
+
+	private final List<Part> pages;
+
+	public Document() {
+		pages = new ArrayList<Part>();
+	}
+
+	public Document(List<Part> pages) {
+		this.pages = pages;
+	}
+
+	private Document(Parcel in) {
+		pages = new ArrayList<Part>();
+
+		in.readList(pages, Part.class.getClassLoader());
+	}
+
+	public List<Part> getPages() {
+		return pages;
+	}
+
+	public void addPage(Part page) {
+		pages.add(page);
+	}
+
+	public Part getPageAt(int index) {
+		return pages.get(index);
+	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeList(pages);
+	}
+
+	public static class Part implements Parcelable {
+
+		public static final Parcelable.Creator<Part> CREATOR = new Parcelable.Creator<Part>() {
+			public Part createFromParcel(Parcel in) {
+				return new Part(in);
+			}
+
+			public Part[] newArray(int size) {
+				return new Part[size];
+			}
+		};
+
+		private final String name;
+		private final String url;
+		private final int index;
+
+		public Part(String name, URI url, int index) {
+			this.name = name;
+			this.url = url.toString();
+			this.index = index;
+		}
+
+		private Part(Parcel in) {
+			name = in.readString();
+			url = in.readString();
+			index = in.readInt();
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String getUrl() {
+			return url;
+		}
+
+		public int getIndex() {
+			return index;
+		}
+
+		@Override
+		public int describeContents() {
+			return 0;
+		}
+
+		@Override
+		public void writeToParcel(Parcel dest, int flags) {
+			dest.writeString(name);
+			dest.writeString(url);
+			dest.writeInt(index);
+		}
+	}
+}
