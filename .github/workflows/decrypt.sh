0e6a6d23a5b2edcf3f5da95912ad07c2aed9a724
marcelblanarik <marcel@indoo.rs>
2019-12-23 11:16:10 +0100

    create file

--- .github/workflows/decrypt.sh
+++ .github/workflows/decrypt.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+touch ../../app/google-services.json
+
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
---output ./app/google-services.json google-services.json.gpg
+--output ./app/google-services.json ./google-services.json.gpg
