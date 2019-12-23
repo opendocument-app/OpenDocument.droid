38908f252702f3ef3b4e25fa0bf0a58ff417d10c
Marcel Blanárik <marcel.blanarik@gmail.com>
2019-12-23 11:39:56 +0100

    Update decrypt.sh

--- .github/workflows/decrypt.sh
+++ .github/workflows/decrypt.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 touch ./app/google-services.json
-pwd
-ls -a
 
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
 --output ./app/google-services.json ./.github/workflows/google-services.json.gpg
