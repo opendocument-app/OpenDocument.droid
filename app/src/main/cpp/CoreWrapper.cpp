683a4b87ebedd6494336ddeb91915b5df28c33b2
Thomas Taschauer <thomas.taschauer@miracl.at>
2021-10-08 18:47:23 +0200

    tmp

--- app/src/main/cpp/CoreWrapper.cpp
+++ app/src/main/cpp/CoreWrapper.cpp
@@ -1,10 +1,11 @@
 #include "CoreWrapper.h"
+#include <string>
+#include <odr/document.h>
+#include <odr/file.h>
+#include <odr/html.h>
 
-#include <odr/Document.h>
-#include <odr/Config.h>
-#include <odr/Meta.h>
-
-std::optional<odr::DocumentNoExcept> document;
+std::optional<odr::DocumentFile> documentFile;
+std::optional<odr::Document> document;
 
 JNIEXPORT jobject JNICALL
 Java_at_tomtasche_reader_background_CoreWrapper_parseNative(JNIEnv *env, jobject instance, jobject options)
@@ -26,34 +27,35 @@ Java_at_tomtasche_reader_background_CoreWrapper_parseNative(JNIEnv *env, jobject
     env->ReleaseStringUTFChars(inputPath, inputPathC);
 
     try {
-        document = odr::DocumentNoExcept::open(inputPathCpp);
         if (!document.has_value()) {
             env->SetIntField(result, errorField, -1);
             return result;
         }
 
-        auto meta = document->meta();
+        odr::DecodedFile file(inputPathCpp);
+        // TODO this throws if it is not a document file!
+        documentFile = file.document_file();
 
         jfieldID passwordField = env->GetFieldID(optionsClass, "password", "Ljava/lang/String;");
         jstring password = (jstring) env->GetObjectField(options, passwordField);
 
-        bool decrypted = !meta.encrypted;
-        if (password != NULL) {
+        bool decrypted = !documentFile->password_encrypted();
+        if (password != nullptr) {
             const auto passwordC = env->GetStringUTFChars(password, &isCopy);
             const auto passwordCpp = std::string(passwordC, env->GetStringUTFLength(password));
             env->ReleaseStringUTFChars(password, passwordC);
 
-            decrypted = document->decrypt(passwordCpp);
-
-            meta = document->meta();
+            decrypted = documentFile->decrypt(passwordCpp);
         }
+
+        document = documentFile->document();
 
         if (!decrypted) {
             env->SetIntField(result, errorField, -2);
             return result;
         }
 
-        const auto extensionCpp = meta.typeAsString();
+        const auto extensionCpp = documentFile->file_meta().type_as_string();
         const auto extensionC = extensionCpp.c_str();
         jstring extension = env->NewStringUTF(extensionC);
 
@@ -63,10 +65,10 @@ Java_at_tomtasche_reader_background_CoreWrapper_parseNative(JNIEnv *env, jobject
         jfieldID editableField = env->GetFieldID(optionsClass, "editable", "Z");
         jboolean editable = env->GetBooleanField(options, editableField);
 
-        odr::Config config = {};
+        odr::HtmlConfig config;
         config.editable = editable;
-        config.entryCount = 1;
-        config.tableLimitRows = 10000;
+        config.entry_count = 1;
+        config.table_limit = {10000, 500};
 
         jfieldID outputPathField = env->GetFieldID(optionsClass, "outputPath", "Ljava/lang/String;");
         jstring outputPath = (jstring) env->GetObjectField(options, outputPathField);
@@ -83,76 +85,37 @@ Java_at_tomtasche_reader_background_CoreWrapper_parseNative(JNIEnv *env, jobject
 
         jfieldID ooxmlField = env->GetFieldID(optionsClass, "ooxml", "Z");
         jboolean ooxml = env->GetBooleanField(options, ooxmlField);
-        if (!ooxml) {
-            if (meta.type == odr::FileType::OPENDOCUMENT_TEXT) {
-                jstring pageName = env->NewStringUTF("Document");
-                env->CallBooleanMethod(pageNames, addMethod, pageName);
+        if (!ooxml && documentFile->file_type() == odr::FileType::office_open_xml_document) {
+            // TODO fail fast if ooxml file is given but disabled
+            env->SetIntField(result, errorField, -5);
+            return result;
+        }
 
-                outputPathCpp = outputPathCpp + "0.html";
+        if (document->document_type() == odr::DocumentType::text) {
+            jstring pageName = env->NewStringUTF("Document");
+            env->CallBooleanMethod(pageNames, addMethod, pageName);
 
-                bool translated = document->translate(outputPathCpp, config);
-                if (!translated) {
-                    env->SetIntField(result, errorField, -4);
-                    return result;
-                }
-            } else if (meta.type == odr::FileType::OPENDOCUMENT_SPREADSHEET || meta.type == odr::FileType::OPENDOCUMENT_PRESENTATION || meta.type == odr::FileType::OPENDOCUMENT_GRAPHICS) {
-                int i = 0;
-                // TODO: this could fail for HUGE documents with hundreds of pages
-                // https://stackoverflow.com/a/24292867/198996
-                for (auto page = meta.entries.begin(); page != meta.entries.end(); page++) {
-                    jstring pageName = env->NewStringUTF(page->name.c_str());
-                    env->CallBooleanMethod(pageNames, addMethod, pageName);
+            outputPathCpp = outputPathCpp + "0.html";
 
-                    const auto entryOutputPath = outputPathCpp + std::to_string(i) + ".html";
-                    config.entryOffset = i;
+            odr::html::translate(*document, outputPathCpp, config);
+        } else if (document->document_type() == odr::DocumentType::spreadsheet) {
+            auto cursor = document->root_element();
 
-                    bool translated = document->translate(entryOutputPath, config);
-                    if (!translated) {
-                        env->SetIntField(result, errorField, -4);
-                        return result;
-                    }
+            cursor.for_each_child([&](odr::DocumentCursor &cursor, std::uint32_t i) {
+                auto sheet = cursor.element().sheet();
 
-                    i++;
-                }
-            } else {
-                env->SetIntField(result, errorField, -5);
-                return result;
-            }
+                jstring sheetName = env->NewStringUTF(sheet.name().c_str());
+                env->CallBooleanMethod(pageNames, addMethod, sheetName);
+
+                const auto entryOutputPath = outputPathCpp + std::to_string(i) + ".html";
+                config.entry_offset = i;
+
+                // TODO: trycatch, return -4
+                odr::html::translate(*document, entryOutputPath, config);
+            });
         } else {
-            if (meta.type == odr::FileType::OFFICE_OPEN_XML_DOCUMENT) {
-                jstring pageName = env->NewStringUTF("Text document");
-                env->CallBooleanMethod(pageNames, addMethod, pageName);
-
-                outputPathCpp = outputPathCpp + "0.html";
-
-                bool translated = document->translate(outputPathCpp, config);
-                if (!translated) {
-                    env->SetIntField(result, errorField, -4);
-                    return result;
-                }
-            } else if (meta.type == odr::FileType::OFFICE_OPEN_XML_WORKBOOK || meta.type == odr::FileType::OFFICE_OPEN_XML_PRESENTATION) {
-                int i = 0;
-                // TODO: this could fail for HUGE documents with hundreds of pages
-                // https://stackoverflow.com/a/24292867/198996
-                for (auto page = meta.entries.begin(); page != meta.entries.end(); page++) {
-                    jstring pageName = env->NewStringUTF(page->name.c_str());
-                    env->CallBooleanMethod(pageNames, addMethod, pageName);
-
-                    const auto entryOutputPath = outputPathCpp + std::to_string(i) + ".html";
-                    config.entryOffset = i;
-
-                    bool translated = document->translate(entryOutputPath, config);
-                    if (!translated) {
-                        env->SetIntField(result, errorField, -4);
-                        return result;
-                    }
-
-                    i++;
-                }
-            } else {
-                env->SetIntField(result, errorField, -5);
-                return result;
-            }
+            env->SetIntField(result, errorField, -5);
+            return result;
         }
     } catch (...) {
         env->SetIntField(result, errorField, -3);
@@ -188,9 +151,7 @@ Java_at_tomtasche_reader_background_CoreWrapper_backtranslateNative(JNIEnv *env,
         auto htmlDiffCpp = std::string(htmlDiffC, env->GetStringUTFLength(htmlDiff));
         env->ReleaseStringUTFChars(htmlDiff, htmlDiffC);
 
-        const auto meta = document->meta();
-
-        const auto extension = meta.typeAsString();
+        const auto extension = documentFile->file_meta().type_as_string();
         const auto outputPathCpp = outputPathPrefixCpp + "." + extension;
         const char *outputPathC = outputPathCpp.c_str();
         jstring outputPath = env->NewStringUTF(outputPathC);
@@ -198,17 +159,11 @@ Java_at_tomtasche_reader_background_CoreWrapper_backtranslateNative(JNIEnv *env,
         jfieldID outputPathField = env->GetFieldID(resultClass, "outputPath", "Ljava/lang/String;");
         env->SetObjectField(result, outputPathField, outputPath);
 
-        bool success = document->edit(htmlDiffCpp);
-        if (!success) {
-            env->SetIntField(result, errorField, -6);
-            return result;
-        }
+        // TODO: trycatch, return -6
+        odr::html::edit(*document, htmlDiffCpp);
 
-        success = document->save(outputPathCpp);
-        if (!success) {
-            env->SetIntField(result, errorField, -7);
-            return result;
-        }
+        // TODO: trycatch, return -7
+        document->save(outputPathCpp);
     } catch (...) {
         env->SetIntField(result, errorField, -3);
         return result;
