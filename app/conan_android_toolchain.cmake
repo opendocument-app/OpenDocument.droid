8ac6398824b2704ec01066ae56492156feb939f5
Andreas Stefl <stefl.andreas@gmail.com>
2024-05-26 22:47:03 +0200

    relwithdebinfo build

--- app/conan_android_toolchain.cmake
+++ app/conan_android_toolchain.cmake
@@ -1,17 +1,17 @@
 # During multiple stages of CMake configuration, the toolchain file is processed and command-line
 # variables may not be always available. The script exits prematurely if essential variables are absent.
 
-if ( NOT ANDROID_ABI OR NOT CMAKE_BUILD_TYPE )
+if ( NOT ANDROID_ABI )
   return()
 endif()
 if(${ANDROID_ABI} STREQUAL "x86_64")
-  include("${CMAKE_CURRENT_LIST_DIR}/build/x86_64/${CMAKE_BUILD_TYPE}/generators/conan_toolchain.cmake")
+  include("${CMAKE_CURRENT_LIST_DIR}/build/x86_64/generators/conan_toolchain.cmake")
 elseif(${ANDROID_ABI} STREQUAL "x86")
-  include("${CMAKE_CURRENT_LIST_DIR}/build/x86/${CMAKE_BUILD_TYPE}/generators/conan_toolchain.cmake")
+  include("${CMAKE_CURRENT_LIST_DIR}/build/x86/generators/conan_toolchain.cmake")
 elseif(${ANDROID_ABI} STREQUAL "arm64-v8a")
-  include("${CMAKE_CURRENT_LIST_DIR}/build/armv8/${CMAKE_BUILD_TYPE}/generators/conan_toolchain.cmake")
+  include("${CMAKE_CURRENT_LIST_DIR}/build/armv8/generators/conan_toolchain.cmake")
 elseif(${ANDROID_ABI} STREQUAL "armeabi-v7a")
-  include("${CMAKE_CURRENT_LIST_DIR}/build/armv7/${CMAKE_BUILD_TYPE}/generators/conan_toolchain.cmake")
+  include("${CMAKE_CURRENT_LIST_DIR}/build/armv7/generators/conan_toolchain.cmake")
 else()
   message(FATAL "Not supported configuration")
 endif()
