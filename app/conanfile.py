53769d18435ee5f547c60cd35d0f6d19e7164e43
Vilius Sutkus '89 <Vilius@ViliusSutkus89.com>
2024-08-15 12:31:48 +0300

    Update pdf2htmlEX and it's asset collection mechanism

---
+++ app/conanfile.py
@@ -0,0 +1,22 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps
+
+required_conan_version = ">=2.0.6"
+
+
+class pdf2htmlEXConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    # Remove pdf2htmlex dep once https://github.com/opendocument-app/OpenDocument.core/pull/380 is merged
+    requires = "odrcore/4.1.1@", "pdf2htmlex/0.18.8.rc1-20240814-git"
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+
+        # @TODO: figure out how to use POPPLER_DATA_DIR exported by poppler-data
+        tc.variables["POPPLER_DATA_RES_DIR"] = self.dependencies['poppler-data'].cpp_info.resdirs[0]
+        tc.variables["PDF2HTMLEX_RES_DIR"] = self.dependencies['pdf2htmlex'].cpp_info.resdirs[0]
+        tc.variables["FONTCONFIG_RES_DIR"] = self.dependencies['fontconfig'].cpp_info.resdirs[0]
+        tc.generate()
+
