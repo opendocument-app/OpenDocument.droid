b3046a53b226b1bf39b1e19d2e04ede06431b9d0
Andreas Stefl <stefl.andreas@gmail.com>
2025-08-04 21:23:43 +0200

    bump core again

--- app/src/main/cpp/CoreWrapper.cpp
+++ app/src/main/cpp/CoreWrapper.cpp
@@ -94,7 +94,6 @@ Java_at_tomtasche_reader_background_CoreWrapper_setGlobalParams(JNIEnv *env, jcl
     odr::GlobalParams::set_fontconfig_data_path(fontconfigDataPath);
     odr::GlobalParams::set_poppler_data_path(popplerDataPath);
     odr::GlobalParams::set_pdf2htmlex_data_path(pdf2htmlexDataPath);
-    odr::GlobalParams::set_custom_tmpfile_path(customTmpfilePath);
 
     setenv("TMPDIR", customTmpfilePath.c_str(), 1);
 }
